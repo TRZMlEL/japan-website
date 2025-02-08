697e5ac6e1f86c2a3d46e7b37352495a6fb5bc54
TRZMlEL <mateusztrzmiel2104@gmail.com>
2025-02-09 00:35:40 +0100

    🌍 Update path check to include japan-website root

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ japonia = document.querySelector('#japonia');
 
 
 
-if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "/japan-website/index.html") {
+if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "/japan-website/index.html" || window.location.pathname === "/japan-website/") {
   main.style.overflowY = 'hidden';
   main.style.marginLeft = 'calc(15% + 0.3rem)';
   japonia.addEventListener('animationend', () => {
