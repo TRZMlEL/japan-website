930b1d370b13f2bd960726b8a2cac429d614d448
TRZMlEL <mateusztrzmiel2104@gmail.com>
2025-02-09 00:33:09 +0100

    🌍 Update path check to include Japan website index

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ japonia = document.querySelector('#japonia');
 
 
 
-if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "/japan-website/index.html") {
   main.style.overflowY = 'hidden';
   main.style.marginLeft = 'calc(15% + 0.3rem)';
   japonia.addEventListener('animationend', () => {
