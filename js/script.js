0d2254cbaa87381be2c52a0f631979a31bdb7b99
TRZMlEL <mateusztrzmiel2104@gmail.com>
2025-02-11 23:08:39 +0100

    ✨ Implement smooth scrolling for map areas and enhance tourism section with detailed city information

--- js/script.js
+++ js/script.js
@@ -26,3 +26,14 @@ if (window.location.pathname === "/index.html" || window.location.pathname === "
     }
   });
 }
+
+document.querySelectorAll("area").forEach(area => {
+  area.addEventListener("click", function(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+  });
+});
